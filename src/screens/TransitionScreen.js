86717e38cb67092e617fe792d7148682e1a82e5a
José Rodrigo Barrera <70623939+bar20807@users.noreply.github.com>
2024-06-13 23:18:26 -0600

    fix-transition-animation

--- src/screens/TransitionScreen.js
+++ src/screens/TransitionScreen.js
@@ -1,13 +1,25 @@
 // src/screens/TransitionScreen.js
 import React, { useEffect } from "react";
 import { View, StyleSheet, Image, Dimensions } from "react-native";
+import Animated, {
+  Easing,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 const { width, height } = Dimensions.get("window");
 
 const TransitionScreen = ({ navigation, route }) => {
   const { nextScreen, ...params } = route.params;
+  const opacity = useSharedValue(0);
 
   useEffect(() => {
+    opacity.value = withTiming(1, {
+      duration: 2000,
+      easing: Easing.inOut(Easing.ease),
+    });
+
     const timer = setTimeout(() => {
       navigation.navigate(nextScreen, params);
     }, 2000);
@@ -15,12 +27,20 @@ const TransitionScreen = ({ navigation, route }) => {
     return () => clearTimeout(timer);
   }, [navigation, nextScreen, params]);
 
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../../assets/images/logo_blanco_transicion.png")}
-        style={styles.logo}
-      />
+      <Animated.View style={[styles.logoContainer, animatedStyles]}>
+        <Image
+          source={require("../../assets/images/logo_blanco_transicion.png")}
+          style={styles.logo}
+        />
+      </Animated.View>
     </View>
   );
 };
@@ -29,6 +49,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#87CEEB", // Sky blue color
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoContainer: {
+    width: width,
+    height: height,
     justifyContent: "center",
     alignItems: "center",
   },
